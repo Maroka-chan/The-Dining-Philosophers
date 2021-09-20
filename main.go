1aba4bf9152e683999f37b6f04a3cc76099e8beb
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-09-20 21:10:51 +0200

    Add enum & remove commented code

--- main.go
+++ main.go
@@ -6,18 +6,22 @@ import (
 	"time"
 )
 
-const PNUM int = 5
+const philosopherCount int = 5
+
+const (
+	NotInUse = iota
+)
 
 func main() {
-	var forks [PNUM]*Fork
-	var philosophers [PNUM]*Philosopher
+	var forks [philosopherCount]*Fork
+	var philosophers [philosopherCount]*Philosopher
 
 	for i := 0; i < 5; i++ {
-		forks[i] = &Fork{i, 0, 0, nil, make(chan int), make(chan int), sync.Mutex{}}
+		forks[i] = &Fork{i, 0, NotInUse, nil, make(chan int), make(chan int), sync.Mutex{}}
 	}
 
 	for i := 0; i < 5; i++ {
-		philosophers[i] = &Philosopher{i, 0, forks[i], forks[(i+1)%PNUM], make(chan int), make(chan int)}
+		philosophers[i] = &Philosopher{i, 0, forks[i], forks[(i+1)%philosopherCount], make(chan int), make(chan int)}
 	}
 
 	for _, fork := range forks {
@@ -31,11 +35,6 @@ func main() {
 
 	go philosophers[4].QueryLoop()
 	go philosophers[4].Run_rev()
-
-	// for _, philosopher := range philosophers {
-	// 	go philosopher.QueryLoop()
-	// 	go philosopher.Run()
-	// }
 
 	for {
 		time.Sleep(time.Second)
